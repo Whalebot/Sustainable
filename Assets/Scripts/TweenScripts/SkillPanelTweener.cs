e0e7644b06024f2c4a89b6026228696ece8280af
Eplundare <aelozanoa@gmail.com>
2020-03-10 10:31:42 +0100

    Added animation to SkillTab
    Added animation to SkillTab

---
+++ Assets/Scripts/TweenScripts/SkillPanelTweener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillPanelTweener : MonoBehaviour
+{
+    public float distance = 0f;
+    public float speed = 0f;
+    public float delay = 0f;
+
+    //public MoneyManager moneyManager;
+
+    public AnimationCurve curve;
+    //public LeanTweenType easeType;
+
+
+    public void SkillPanelUpper()
+    {
+        LeanTween.moveLocalY(gameObject, distance, speed).setEase(curve);
+    }
+
+    public void SkillPanelDowner()
+    {
+        LeanTween.moveLocalY(gameObject, -distance, speed).setEase(curve);
+    }
+
+}
