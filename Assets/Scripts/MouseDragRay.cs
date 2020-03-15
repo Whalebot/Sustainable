c9bbef67102cb7e51404aac4802b120836c9c220
Eplundare <aelozanoa@gmail.com>
2020-03-15 12:42:46 +0100

    Added cinemachine
    zoom, confinement (placeholder), interaction with drag

--- Assets/Scripts/MouseDragRay.cs
+++ Assets/Scripts/MouseDragRay.cs
@@ -7,6 +7,7 @@ public class MouseDragRay : MonoBehaviour
 
     public bool m_Drag;
     public Vector3 m_MouseDownPos = new Vector3 (0f,0f,0f);
+    public GameObject cameraParent;
 
     public void Update()
     {
@@ -22,7 +23,8 @@ public class MouseDragRay : MonoBehaviour
             {
                 GroundPlane.Raycast(MouseRay, out HitDist);
                 Vector3 CurClickPos = MouseRay.GetPoint(HitDist);
-                Camera.main.transform.position += m_MouseDownPos - CurClickPos;
+                cameraParent.transform.position += m_MouseDownPos - CurClickPos;
+
             }
             else //if (Input.GetMouseButtonDown(0))
             {
@@ -35,5 +37,34 @@ public class MouseDragRay : MonoBehaviour
             m_Drag = false;
     }
 
-    
+    //public bool m_Drag;
+    //public Vector3 m_MouseDownPos = new Vector3(0f, 0f, 0f);
+
+    //public void Update()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        float HitDist;
+    //        Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //        Vector3 GroundNormal = new Vector3(0.0f, 1.0f, 0.0f),
+    //                GroundPoint = new Vector3(0.0f, 0.0f, 0.0f);
+    //        Plane GroundPlane = new Plane(GroundNormal, GroundPoint);
+
+    //        if (m_Drag)
+    //        {
+    //            GroundPlane.Raycast(MouseRay, out HitDist);
+    //            Vector3 CurClickPos = MouseRay.GetPoint(HitDist);
+    //            Camera.main.transform.position += m_MouseDownPos - CurClickPos;
+    //        }
+    //        else //if (Input.GetMouseButtonDown(0))
+    //        {
+    //            GroundPlane.Raycast(MouseRay, out HitDist);
+    //            m_MouseDownPos = MouseRay.GetPoint(HitDist);
+    //            m_Drag = true;
+    //        }
+    //    }
+    //    else
+    //        m_Drag = false;
+    //}
+
 }
