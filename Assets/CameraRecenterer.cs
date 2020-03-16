15920e87bb33989a174ddbb24aa08c36fb833e8f
Eplundare <aelozanoa@gmail.com>
2020-03-16 18:37:48 +0100

    Cam recenter working

---
+++ Assets/CameraRecenterer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRecenterer : MonoBehaviour
+{
+    public Transform cinemachinePos;
+
+    public Transform camCenter;
+    public float speed;
+    public AnimationCurve curve;
+
+    public void Start()
+    {
+        camCenter.transform.position = cinemachinePos.position;
+    }
+
+    public void Recenter()
+    {
+        Vector3 camCenterVector = new Vector3(camCenter.position.x, camCenter.position.y, camCenter.position.z);
+        LeanTween.moveLocal(gameObject, camCenterVector, speed).setEase(curve);
+    }
+}
