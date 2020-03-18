db4d14317725a2e0061b685906b9f264b4811d07
Eplundare <aelozanoa@gmail.com>
2020-03-18 21:08:54 +0100

    Implemented code for UI Hover. Graphics are sketch

---
+++ Assets/Scripts/UiFollowMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiFollowMouse : MonoBehaviour
+{
+    public RectTransform movingDiv;
+    public Vector3 offset;
+    public RectTransform zBasis;
+    public Camera cam;
+
+    public float zValue = 0f;
+
+    public void Update()
+    {
+        MoveUi();
+    }
+
+    public void MoveUi()
+    {
+        Vector3 pos = Input.mousePosition + offset;
+        //pos.z = zBasis.position.z;
+        pos.z = zValue;
+
+        movingDiv.position = cam.ScreenToWorldPoint(pos);
+    }
+
+}
---
+++ Assets/Scripts/UiInfoHoverer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UiInfoHoverer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject specificDiv;
+
+    private bool mouse_over = false;
+
+    public void Start()
+    {
+        specificDiv.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (mouse_over)
+        {
+            Debug.Log("Mouse Over");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouse_over = true;
+        Debug.Log("Mouse enter");
+        specificDiv.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouse_over = false;
+        Debug.Log("Mouse exit");
+        specificDiv.gameObject.SetActive(false);
+
+    }
+
+}
