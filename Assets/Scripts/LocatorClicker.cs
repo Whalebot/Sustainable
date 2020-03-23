64b69a1d2c64b5e627c481d46063eb762cc99416
Eplundare <aelozanoa@gmail.com>
2020-03-23 19:50:23 +0100

    Implemented animation of Locator and TradeOffWin and UpgradeWin

---
+++ Assets/Scripts/LocatorClicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocatorClicker : MonoBehaviour
+{
+    public GameObject tradeOffWindow;
+
+    public Vector3 windowScaleIntro;
+    public Vector3 windowScaleOutro;
+    public float distanceInside;
+    public float distanceOutside;
+    public float speed;
+
+    public AnimationCurve curveIn;
+    public AnimationCurve curveOut;
+
+    public void Start()
+    {
+        tradeOffWindow.gameObject.SetActive(false);
+    }
+
+    public void OnMouseDown()
+    {
+        tradeOffWindow.gameObject.SetActive(true);
+        LeanTween.scale(tradeOffWindow, windowScaleIntro, speed).setEase(curveIn);
+        LeanTween.moveLocalY(tradeOffWindow, distanceInside, speed).setEase(curveIn);
+    }
+
+    public void CloseTradeOffWindow()
+    {
+        LeanTween.scale(tradeOffWindow, windowScaleOutro, speed).setEase(curveOut);
+        LeanTween.moveLocalY(tradeOffWindow, distanceOutside, speed).setEase(curveOut);
+    }
+}
---
+++ Assets/Scripts/TweenScripts/LocatorAnimator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LocatorAnimator : MonoBehaviour
+{
+    public GameObject Pivot;
+
+    public Vector3 mouseUpScaleIntensity;
+    public float mouseUpScaleTime;
+
+    public GameObject locatorColored;
+    public GameObject locatorNormalColor;
+
+    public int loops;
+    public AnimationCurve downCurve;
+
+    public float shrinkSpeed;
+    public float unshrinkSpeed;
+    public float unshrinkDelay;
+    public AnimationCurve shrinkCurve;
+
+    public void Start()
+    {
+        locatorNormalColor.gameObject.SetActive(true);
+        locatorColored.gameObject.SetActive(false);
+
+    }
+
+    public void OnMouseDown()
+    {
+        LeanTween.scale(Pivot, mouseUpScaleIntensity, mouseUpScaleTime).setEase(downCurve).setLoopPingPong(loops);
+    }
+
+    public void OnMouseOver()
+    {
+        //LeanTween.scale(Pivot, mouseOverIntensity, mouseOverExitTime).setEase(overExitCurve);
+        locatorNormalColor.gameObject.SetActive(false);
+        locatorColored.gameObject.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        //LeanTween.scale(Pivot, mouseExitIntensity, mouseOverExitTime).setEase(overExitCurve);
+        locatorNormalColor.gameObject.SetActive(true);
+        locatorColored.gameObject.SetActive(false);
+
+
+    }
+
+    public void ShrinkLocator()
+    {
+        Vector3 shrinkScale = new Vector3 (0f, 0f, 0f);
+        LeanTween.scale(Pivot, (shrinkScale), shrinkSpeed).setEase(shrinkCurve);
+    }
+
+    public void UnshrinkLocator()
+    {
+        Vector3 shrinkScale = new Vector3(0f, 0f, 0f);
+        Vector3 unshrinkScale = new Vector3(1f, 1f, 1f);
+        //LeanTween.scale(Pivot, (shrinkScale), shrinkSpeed).setDelay(1f);
+        LeanTween.scale(Pivot, (unshrinkScale), unshrinkSpeed).setDelay(unshrinkDelay).setEase(shrinkCurve);
+
+    }
+}
