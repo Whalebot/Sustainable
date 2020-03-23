2fe89de85cf2a83bd56a3b95fea6887ba9e938ac
Eplundare <aelozanoa@gmail.com>
2020-03-23 12:58:04 +0100

    More UI implementation

--- Assets/Scripts/ListSwitcher.cs
+++ Assets/Scripts/ListSwitcher.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class ListSwitcher : MonoBehaviour
 {
     public GameObject list;
+    public GameObject UpArrow;
+    public GameObject DownArrow;
     public bool switcher;
 
     public void Start()
     {
         switcher = false;
         list.gameObject.SetActive(false);
+        DownArrow.gameObject.SetActive(true);
+        UpArrow.gameObject.SetActive(false);
     }
 
     public void switchList()
     {
         switcher = !switcher;
         list.gameObject.SetActive(switcher);
+        if (switcher == false)
+        {
+            DownArrow.gameObject.SetActive(true);
+            UpArrow.gameObject.SetActive(false);
+        }
+        else
+        {
+            DownArrow.gameObject.SetActive(false);
+            UpArrow.gameObject.SetActive(true);
+
+        }
 
     }
 }
