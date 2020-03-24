89fc1fe6f964f81e5bdc35f860a508398cd33bfa
Eplundare <aelozanoa@gmail.com>
2020-03-24 11:49:07 +0100

    Improved UI of Descriptor (tradeOff and Upgrade)

---
+++ Assets/Scripts/ShadowResizer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowResizer : MonoBehaviour
+{
+    public RectTransform targetRt;
+    public RectTransform shadowRt;
+
+    public void Update()
+    {
+        shadowRt.sizeDelta = new Vector2(targetRt.sizeDelta.x, targetRt.sizeDelta.y);
+    }
+}
