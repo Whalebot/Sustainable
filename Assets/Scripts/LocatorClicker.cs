3593bf2ee12d2908fba12f8199f357692d27faf2
Eplundare <aelozanoa@gmail.com>
2020-04-01 00:25:05 +0200

    Implemented AutomatorDiv (for transport of food). Also, applied some BLOCKERS 3D.

--- Assets/Scripts/LocatorClicker.cs
+++ Assets/Scripts/LocatorClicker.cs
@@ -33,6 +33,15 @@ public class LocatorClicker : MonoBehaviour
     public GameObject nameTag;
     public bool mouse_down;
 
+    //REF FOR POPUP.
+    //public PopUpScaleTweener automatorDiv;
+    public GameObject automatorDiv;
+    public bool winHasPopUp;
+    public Vector3 popScaleStart = new Vector3(0f, 0f, 0f);
+    public Vector3 popScaleBig;
+    public float popUpSpeed;
+    public float popUpDelay;
+    public AnimationCurve popUpEase;
 
 
 
@@ -42,6 +51,14 @@ public class LocatorClicker : MonoBehaviour
         LeanTween.scale(tradeOffWindow, windowScaleOutro, 0f); //This starts the TradeOffWindow in windowScaleOutro scale.
 
         nameTag.gameObject.SetActive(false);
+
+        //START FOR POPUP
+        popScaleBig = new Vector3(1f, 1f, 1f);
+        if (winHasPopUp == true)
+        {
+            LeanTween.scale(automatorDiv, popScaleStart, 0f);
+
+        }
 
     }
 
@@ -63,6 +80,12 @@ public class LocatorClicker : MonoBehaviour
             //THIS CHANGES CAM POSITION.
             Vector3 camCenterVector = new Vector3(islandPos.position.x, islandPos.position.y, islandPos.position.z);
             LeanTween.moveLocal(camParent, camCenterVector, camSpeed).setEase(curveEaseInOut);
+
+            //THIS POPS UP AUTOMATORDIV.
+            if (winHasPopUp == true)
+            {
+                /*automatorDiv.*/PopUpScaler();
+            }
         }
         else if (isPolicies == true)
         {
@@ -91,6 +114,12 @@ public class LocatorClicker : MonoBehaviour
     {
         LeanTween.scale(tradeOffWindow, windowScaleOutro, speed).setEase(curveOut);
         LeanTween.moveLocalY(tradeOffWindow, distanceOutside, outSpeed).setEase(curveOut);
+
+        //THIS POPS DOWN AUTOMATORDIV.
+        if (winHasPopUp == true)
+        {
+            /*automatorDiv.*/PopDownScaler();
+        }
     }
 
     public void CloseOtherWindows()
@@ -125,6 +154,17 @@ public class LocatorClicker : MonoBehaviour
     {
         nameTag.gameObject.SetActive(false);
         mouse_down = false;
+
+    }
+
+    public void PopUpScaler()
+    {
+        LeanTween.scale(automatorDiv, popScaleBig, speed).setDelay(popUpDelay).setEase(popUpEase);
+    }
+
+    public void PopDownScaler()
+    {
+        LeanTween.scale(automatorDiv, popScaleStart, 0f).setDelay(popUpDelay).setEase(popUpEase);
 
     }
 
---
+++ Assets/Scripts/TweenScripts/PopUpScaleTweener.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpScaleTweener : MonoBehaviour
+{
+    public Vector3 scaleStart = new Vector3 (0f, 0f, 0f);
+    public Vector3 scaleBig = new Vector3(1f, 1f, 1f);
+
+    public float speed;
+    public float delay;
+    public AnimationCurve ease;
+
+    public void Start()
+    {
+        LeanTween.scale(gameObject, scaleStart, 0f);
+
+    }
+
+    public void PopUpScaler()
+    {
+        LeanTween.scale(gameObject, scaleBig, speed).setDelay(delay).setEase(ease);
+    }
+
+    public void PopDownScaler()
+    {
+        LeanTween.scale(gameObject, scaleStart, 0f).setDelay(delay).setEase(ease);
+
+    }
+}
