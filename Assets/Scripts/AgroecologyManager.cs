ebf7987fc10185951c2c2ce9bb703dfd4ffbc4a3
Eplundare <aelozanoa@gmail.com>
2020-04-14 15:50:10 +0200

    Added batch of cataclysm news

---
+++ Assets/Scripts/AgroecologyManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgroecologyManager : MonoBehaviour
+{
+    public GameObject offButt01;
+    public GameObject offButt02;
+    public bool oneIsDeactivated;
+    public bool twoIsDeactivated;
+
+    public void DeactivateOne()
+    {
+        oneIsDeactivated = true;
+    }
+    public void DeactivateTwo()
+    {
+        twoIsDeactivated = true;
+    }
+
+    public void Update()
+    {
+        if (oneIsDeactivated == true)
+        {
+            offButt01.gameObject.SetActive(false);
+        }
+        else if (oneIsDeactivated == false)
+        {
+            offButt01.gameObject.SetActive(true);
+
+        }
+
+        if (twoIsDeactivated == true)
+        {
+            offButt02.gameObject.SetActive(false);
+        }
+        else if (twoIsDeactivated == false)
+        {
+            offButt02.gameObject.SetActive(true);
+
+        }
+    }
+}
