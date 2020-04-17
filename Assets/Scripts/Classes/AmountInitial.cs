eb3456acd07dbe9dd3459f91bd16b3ef0e6cb498
Eplundare <aelozanoa@gmail.com>
2020-04-18 00:04:47 +0200

    Initial values: Stopped at Upgrades.

--- Assets/Scripts/Classes/AmountInitial.cs
+++ Assets/Scripts/Classes/AmountInitial.cs
@@ -6,17 +6,102 @@ using TMPro;
 public class AmountInitial : MonoBehaviour
 {
     public TextMeshProUGUI inputPlaceholder;
+    public TextMeshProUGUI txt;
     public float inputAmount;
+    //public bool affectsMultiple;
+    public bool affectsSomeRes;
+    public Amount someResAmount;
+    public bool affectsTrades;
+    public bool affectsAuto;
+    public TradeOffDescriptorElem[] trades;
+    public bool affectsUpgrades;
+    public UpgradeDescriptorElem[] upgrades;
+    //public Resource resource;
+    //public ResourcePassive resPass;
+    //public bool 
+    //public string emptyString;
     //public float defaultAmount;
 
     public void Awake()
     {
         //defaultAmount = float.Parse(inputPlaceholder.text);
         inputAmount = float.Parse(inputPlaceholder.text);
+
+        if (affectsSomeRes == true)
+        {
+            someResAmount.amountFloat = float.Parse(inputPlaceholder.text);
+        }
+
+        else if (affectsTrades == true)
+        {
+            if (affectsAuto == false)
+            {
+                for (int i = 0; i < trades.Length; i++)
+                {
+                    trades[i].autoFloat = float.Parse(inputPlaceholder.text);
+                }
+            }
+
+            else if (affectsAuto == false)
+            {
+                for (int i = 0; i < trades.Length; i++)
+                {
+                    trades[i].tradeFloat = float.Parse(inputPlaceholder.text);
+                }
+            }
+
+        }
+
+        else if (affectsUpgrades == true)
+        {
+            for (int i = 0; i < upgrades.Length; i++)
+            {
+                upgrades[i].requirementFloat = float.Parse(inputPlaceholder.text);
+            }
+        }
     }
 
     public void Text_Changed(string newText)
     {
         inputAmount = float.Parse(newText);
+
+        if (affectsSomeRes == true)
+        {
+            someResAmount.amountFloat = float.Parse(newText);
+        }
+
+        else if (affectsTrades == true)
+        {
+            if (affectsAuto == false)
+            {
+                for (int i = 0; i < trades.Length; i++)
+                {
+                    trades[i].autoFloat = float.Parse(newText);
+                }
+            }
+
+            else if (affectsAuto == false)
+            {
+                for (int i = 0; i < trades.Length; i++)
+                {
+                    trades[i].tradeFloat = float.Parse(newText);
+                }
+            }
+            
+        }
+
+        else if (affectsUpgrades == true)
+        {
+            for (int i = 0; i < upgrades.Length; i++)
+            {
+                upgrades[i].requirementFloat = float.Parse(newText);
+            }
+        }
+
     }
+
+    //public void Default()
+    //{
+    //    txt.text = emptyString;
+    //}
 }
