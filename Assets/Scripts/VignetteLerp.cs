f00a5b1a5553b4626b725d7bf40beae9ec1ff16c
Eplundare <aelozanoa@gmail.com>
2020-04-20 21:41:43 +0200

    Pollution pixelVignette implemented.

---
+++ Assets/Scripts/VignetteLerp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VignetteLerp : MonoBehaviour
+{
+    public ResourcePassive pollutionResPas;
+    public float threshold;
+    public float lerper;
+    public Image vignetteImg;
+    public Color whiteTrans;
+    public Color whiteOpaq;
+
+    public void Start()
+    {
+        whiteTrans = new Color32(255, 255, 255, 0);
+        whiteOpaq = new Color32(255, 255, 255, 255);
+
+    }
+
+    public void Update()
+    {
+        lerper = pollutionResPas.resourceCurrent.amountFloat / threshold;
+        vignetteImg.color = Color.Lerp(whiteTrans, whiteOpaq, lerper);
+
+        
+    }
+}
