852f2d7afdbd8b050ade47196b064c8f1e1243a8
Eplundare <aelozanoa@gmail.com>
2020-04-22 13:32:21 +0200

    Outline: Camera transitions runs smoothly (or very decently)

---
+++ Assets/Scripts/OutlineCameraEqualizer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineCameraEqualizer : MonoBehaviour
+{
+    public Camera duplicatedCamera;
+    public Camera mainCamera;
+    public GameObject mainCamObj;
+    public GameObject dupCamObj;
+    public bool dupCamCaptured;
+
+    public void Start()
+    {
+        StartCoroutine(SearchCamCoroutine());
+
+        //dupCamObj = mainCamObj.transform.GetChild(0).gameObject;
+        //dupCamObj = mainCamObj.transform.Find("Camera_CameraNormalsTexture").gameObject;
+
+        //duplicatedCamera = dupCamObj.gameObject.GetComponent<Camera>();
+
+
+    }
+
+    IEnumerator SearchCamCoroutine()
+    {
+        
+        yield return new WaitForSeconds(1);
+
+        dupCamObj = mainCamObj.transform.GetChild(0).gameObject;
+        duplicatedCamera = dupCamObj.gameObject.GetComponent<Camera>();
+        dupCamCaptured = true;
+
+
+    }
+
+    public void LateUpdate()
+    {
+        if(dupCamCaptured == true)
+        {
+            duplicatedCamera.orthographicSize = mainCamera.orthographicSize;
+        }
+    }
+
+}
