756023db9917c36ea88c182f2228c653fb7ea133
Eplundare <aelozanoa@gmail.com>
2020-04-23 22:38:24 +0200

    Started testing Resource Feedback.

---
+++ Assets/Scripts/Classes/Test/Test_DescriptorElem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_DescriptorElem : MonoBehaviour
+{
+    public Test_Amount amount;
+    public float numberToAdd;
+
+    public void Add()
+    {
+        //amount = new Test_Amount();
+        amount.amountCurrent += numberToAdd;
+    }
+
+    public void Subtract()
+    {
+        //amount = new Test_Amount();
+        amount.amountCurrent -= numberToAdd;
+    }
+}
+
