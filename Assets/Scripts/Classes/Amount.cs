2d905601318668b539580a0011b13f7aba314fc4
Eplundare <aelozanoa@gmail.com>
2020-04-24 17:37:55 +0200

    Implemented resource consumption/production Feedback. Corrected 3dBlockers.

--- Assets/Scripts/Classes/Amount.cs
+++ Assets/Scripts/Classes/Amount.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Amount : MonoBehaviour
@@ -8,13 +9,97 @@ public class Amount : MonoBehaviour
     public string amountKind;
     public bool isProduct;
     public TextMeshProUGUI amountTxt;
-    public float amountFloat;
+
+    public bool usesProperty;
+    public bool isTab;
+    public float _amountFloat;
+    public float _change;
+    public float amountFloat
+    {
+        get { return _amountFloat; }
+        set
+        {
+            //if (usesProperty == true)
+            //{
+                if (value > _amountFloat)
+                {
+                    //lerpTime = 0f;
+                    //currentCol = Color.Lerp(originalCol, lightCol, lerpTime);
+                    if (usesProperty == true)
+                    {
+                        currentCol = Color.Lerp(originalCol, lightCol, 1f);
+                        uCurrentCol = Color.Lerp(uOriginalCol, uLightCol, 1f);
+
+                        StartCoroutine(positiveCoroutine());
+
+                    }
+                    else if (usesProperty == false)
+                    {
+                        Debug.Log("Doesn't use property");
+                    }
+                }
+                else if (value < _amountFloat)
+                {
+                    //lerpTime = 0f;
+                    //currentCol = Color.Lerp(originalCol, darkCol, lerpTime);
+
+                    if (usesProperty == true)
+                    {
+                        currentCol = Color.Lerp(originalCol, darkCol, 1f);
+                        uCurrentCol = Color.Lerp(uOriginalCol, uDarkCol, 1f);
+
+                        StartCoroutine(negativeCoroutine());
+
+                    }
+                    else if (usesProperty == false)
+                    {
+                        Debug.Log("Doesn't use property");
+                    }
+                }
+                _change = _amountFloat - value;
+                _amountFloat = value;
+        } 
+
+            //else if (usesProperty == false)
+            //{
+            //    Debug.Log("Amount does not use property");
+            //}
+               
+        //}
+    }
+
+
     public bool objectIsLvl = false;
     public int onlyLvl; //Only use this reference if GameObj is a milestone Level.
     public bool objectIsMilestone = false;
     public float onlyMilePrev; //Only write here if 
     //public GameObject amountIcon;
     //public List<GameObject> amountIcon = new List<GameObject>();
+
+    // REFS FOR FEEDBACK COLOR CHANGE;
+    public Image fill;
+    public Image fill2;
+    public Color currentCol;
+    public Color originalCol;
+    public Color lightCol;
+    public Color darkCol;
+
+    public Image universalFill;
+    public Color uCurrentCol;
+    public Color uOriginalCol;
+    public Color uLightCol;
+    public Color uDarkCol;
+
+    //public float lerpTime;
+    public float duration;
+    public float waitSec;
+
+    public void Start()
+    {
+        currentCol = originalCol;
+        uCurrentCol = uOriginalCol;
+
+    }
 
     public void Update()
     {
@@ -25,7 +110,55 @@ public class Amount : MonoBehaviour
                 amountFloat = 0f;
             }
         }
+
+        if (usesProperty == true)
+        {
+
+            if (isTab == true)
+            {
+                fill.color = currentCol;
+                universalFill.color = uCurrentCol;
+
+                fill2.color = currentCol;
+
+            }
+            else if (isTab == false)
+            {
+                fill.color = currentCol;
+                universalFill.color = uCurrentCol;
+
+            }
+
+        }
+
     }
+
+    IEnumerator positiveCoroutine()
+    {
+        Debug.Log("amount increased");
+        //currentCol = Color.Lerp(originalCol, lightCol, 0.5f);
+        yield return new WaitForSeconds(waitSec);
+        //lerpTime = 0f;
+        //currentCol = Color.Lerp(lightCol, originalCol, lerpTime);
+        currentCol = Color.Lerp(lightCol, originalCol, 1f);
+        uCurrentCol = Color.Lerp(uLightCol, uOriginalCol, 1f);
+
+
+    }
+
+    IEnumerator negativeCoroutine()
+    {
+        Debug.Log("amount decreased");
+        //currentCol = Color.Lerp(originalCol, darkCol, 0.5f);
+        yield return new WaitForSeconds(waitSec);
+        //lerpTime = 0f;
+        //currentCol = Color.Lerp(darkCol, originalCol, lerpTime);
+        currentCol = Color.Lerp(darkCol, originalCol, 1f);
+        uCurrentCol = Color.Lerp(uDarkCol, uOriginalCol, 1f);
+
+
+    }
+
 
 
 
--- Assets/Scripts/LocatorClicker.cs
+++ Assets/Scripts/LocatorClicker.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class LocatorClicker : MonoBehaviour
 {
     public GameObject tradeOffWindow;
+    public bool usesInvisibles;
+    public GameObject invisTradeWin; // THIS IS FOR INVISIBLE TRADE OFF WINDOW.
     public LocatorClicker[] otherInteractors; // USED TO CLOSE OTHER WINDOWS.
     public bool isPolicies;
     public TestCameraSwitcher cameraSwitcher;
@@ -36,6 +38,7 @@ public class LocatorClicker : MonoBehaviour
     //REF FOR POPUP.
     //public PopUpScaleTweener automatorDiv;
     public GameObject automatorDiv;
+    public GameObject invisAutomator; // THIS IS FOR INVISIBLE AUTOMATOR DIV.
     public bool winHasPopUp;
     public Vector3 popScaleStart = new Vector3(0f, 0f, 0f);
     public Vector3 popScaleBig;
@@ -48,17 +51,42 @@ public class LocatorClicker : MonoBehaviour
     public void Start()
     {
         //tradeOffWindow.gameObject.SetActive(false); Since the window is outside the camera view, it should be fine.
-        LeanTween.scale(tradeOffWindow, windowScaleOutro, 0f); //This starts the TradeOffWindow in windowScaleOutro scale.
 
-        nameTag.gameObject.SetActive(false);
-
-        //START FOR POPUP
-        popScaleBig = new Vector3(1f, 1f, 1f);
-        if (winHasPopUp == true)
+        if (usesInvisibles == true)
         {
-            LeanTween.scale(automatorDiv, popScaleStart, 0f);
+            LeanTween.scale(tradeOffWindow, windowScaleOutro, 0f); //This starts the TradeOffWindow in windowScaleOutro scale.
+            LeanTween.scale(invisTradeWin, windowScaleOutro, 0f); //This starts the TradeOffWindow in windowScaleOutro scale.
+
+            nameTag.gameObject.SetActive(false);
+
+            //START FOR POPUP
+            popScaleBig = new Vector3(1f, 1f, 1f);
+            if (winHasPopUp == true)
+            {
+                LeanTween.scale(automatorDiv, popScaleStart, 0f);
+                LeanTween.scale(invisAutomator, popScaleStart, 0f);
+
+
+            }
 
         }
+        else if (usesInvisibles == false)
+        {
+            LeanTween.scale(tradeOffWindow, windowScaleOutro, 0f); //This starts the TradeOffWindow in windowScaleOutro scale.
+
+            nameTag.gameObject.SetActive(false);
+
+            //START FOR POPUP
+            popScaleBig = new Vector3(1f, 1f, 1f);
+            if (winHasPopUp == true)
+            {
+                LeanTween.scale(automatorDiv, popScaleStart, 0f);
+
+            }
+
+        }
+
+        
 
     }
 
@@ -73,19 +101,47 @@ public class LocatorClicker : MonoBehaviour
         // IF IsPolicies == True, THEN JUMP TO UPGRADE PANEL.
         if (isPolicies == false)
         {
-            tradeOffWindow.gameObject.SetActive(true);
-            LeanTween.scale(tradeOffWindow, windowScaleIntro, speed).setEase(curveIn);
-            LeanTween.moveLocalY(tradeOffWindow, distanceInside, speed).setEase(curveIn);
-
-            //THIS CHANGES CAM POSITION.
-            Vector3 camCenterVector = new Vector3(islandPos.position.x, islandPos.position.y, islandPos.position.z);
-            LeanTween.moveLocal(camParent, camCenterVector, camSpeed).setEase(curveEaseInOut);
-
-            //THIS POPS UP AUTOMATORDIV.
-            if (winHasPopUp == true)
+            if (usesInvisibles == true)
             {
-                /*automatorDiv.*/PopUpScaler();
+                tradeOffWindow.gameObject.SetActive(true);
+                LeanTween.scale(tradeOffWindow, windowScaleIntro, speed).setEase(curveIn);
+                LeanTween.moveLocalY(tradeOffWindow, distanceInside, speed).setEase(curveIn);
+
+                invisTradeWin.gameObject.SetActive(true);
+                LeanTween.scale(invisTradeWin, windowScaleIntro, speed).setEase(curveIn);
+                LeanTween.moveLocalY(invisTradeWin, distanceInside, speed).setEase(curveIn);
+
+                //THIS CHANGES CAM POSITION.
+                Vector3 camCenterVector = new Vector3(islandPos.position.x, islandPos.position.y, islandPos.position.z);
+                LeanTween.moveLocal(camParent, camCenterVector, camSpeed).setEase(curveEaseInOut);
+
+                //THIS POPS UP AUTOMATORDIV.
+                if (winHasPopUp == true)
+                {
+                    /*automatorDiv.*/
+                    PopUpScaler();
+                }
             }
+            else if (usesInvisibles == false)
+            {
+                tradeOffWindow.gameObject.SetActive(true);
+                LeanTween.scale(tradeOffWindow, windowScaleIntro, speed).setEase(curveIn);
+                LeanTween.moveLocalY(tradeOffWindow, distanceInside, speed).setEase(curveIn);
+
+
+                //THIS CHANGES CAM POSITION.
+                Vector3 camCenterVector = new Vector3(islandPos.position.x, islandPos.position.y, islandPos.position.z);
+                LeanTween.moveLocal(camParent, camCenterVector, camSpeed).setEase(curveEaseInOut);
+
+                //THIS POPS UP AUTOMATORDIV.
+                if (winHasPopUp == true)
+                {
+                    /*automatorDiv.*/
+                    PopUpScaler();
+                }
+            }
+
+
         }
         else if (isPolicies == true)
         {
@@ -112,14 +168,37 @@ public class LocatorClicker : MonoBehaviour
 
     public void CloseTradeOffWindow()
     {
-        LeanTween.scale(tradeOffWindow, windowScaleOutro, speed).setEase(curveOut);
-        LeanTween.moveLocalY(tradeOffWindow, distanceOutside, outSpeed).setEase(curveOut);
-
-        //THIS POPS DOWN AUTOMATORDIV.
-        if (winHasPopUp == true)
+        if (usesInvisibles == true)
         {
-            /*automatorDiv.*/PopDownScaler();
+            LeanTween.scale(tradeOffWindow, windowScaleOutro, speed).setEase(curveOut);
+            LeanTween.moveLocalY(tradeOffWindow, distanceOutside, outSpeed).setEase(curveOut);
+
+            LeanTween.scale(invisTradeWin, windowScaleOutro, speed).setEase(curveOut);
+            LeanTween.moveLocalY(invisTradeWin, distanceOutside, outSpeed).setEase(curveOut);
+
+
+            //THIS POPS DOWN AUTOMATORDIV.
+            if (winHasPopUp == true)
+            {
+                /*automatorDiv.*/
+                PopDownScaler();
+            }
         }
+
+        else if (usesInvisibles == false)
+        {
+            LeanTween.scale(tradeOffWindow, windowScaleOutro, speed).setEase(curveOut);
+            LeanTween.moveLocalY(tradeOffWindow, distanceOutside, outSpeed).setEase(curveOut);
+
+            //THIS POPS DOWN AUTOMATORDIV.
+            if (winHasPopUp == true)
+            {
+                /*automatorDiv.*/
+                PopDownScaler();
+            }
+        }
+
+
     }
 
     public void CloseOtherWindows()
@@ -159,12 +238,37 @@ public class LocatorClicker : MonoBehaviour
 
     public void PopUpScaler()
     {
-        LeanTween.scale(automatorDiv, popScaleBig, speed).setDelay(popUpDelay).setEase(popUpEase);
+        if (usesInvisibles == true)
+        {
+            LeanTween.scale(automatorDiv, popScaleBig, speed).setDelay(popUpDelay).setEase(popUpEase);
+            LeanTween.scale(invisAutomator, popScaleBig, speed).setDelay(popUpDelay).setEase(popUpEase);
+
+
+        }
+
+        else if (usesInvisibles == false)
+        {
+            LeanTween.scale(automatorDiv, popScaleBig, speed).setDelay(popUpDelay).setEase(popUpEase);
+
+        }
+
     }
 
     public void PopDownScaler()
     {
-        LeanTween.scale(automatorDiv, popScaleStart, 0f).setDelay(popUpDelay).setEase(popUpEase);
+        if (usesInvisibles == true)
+        {
+            LeanTween.scale(automatorDiv, popScaleStart, 0f).setDelay(popUpDelay).setEase(popUpEase);
+            LeanTween.scale(invisAutomator, popScaleStart, 0f).setDelay(popUpDelay).setEase(popUpEase);
+
+        }
+
+        else if (usesInvisibles == false)
+        {
+            LeanTween.scale(automatorDiv, popScaleStart, 0f).setDelay(popUpDelay).setEase(popUpEase);
+
+        }
+
 
     }
 
