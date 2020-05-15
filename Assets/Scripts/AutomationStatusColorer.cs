775013d719209be11ab0d30e5b95215ad85b9647
Eplundare <aelozanoa@gmail.com>
2020-05-16 01:16:23 +0200

    Energy (main structure) on the works.

---
+++ Assets/Scripts/AutomationStatusColorer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutomationStatusColorer : MonoBehaviour
+{
+    // COLORS
+    public Color inactCol;
+    public Color actCol;
+    public Image autoPseudoButton;
+    public GameObject actObj;
+    public GameObject inactObj;
+
+    public void Start()
+    {
+        autoPseudoButton.color = inactCol;
+    }
+
+    public void Update()
+    {
+        if (inactObj.activeInHierarchy == true)
+        {
+            autoPseudoButton.color = inactCol;
+
+        }
+        else if (actObj.activeInHierarchy == true)
+        {
+            autoPseudoButton.color = actCol;
+
+        }
+    }
+}
