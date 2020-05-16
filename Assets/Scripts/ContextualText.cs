373c0fac09211132dce122cd6c1458dc43d41407
Eplundare <aelozanoa@gmail.com>
2020-05-16 18:39:16 +0200

    WasteManagement New Structure is almost complete.

---
+++ Assets/Scripts/ContextualText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ContextualText : MonoBehaviour
+{
+    public TextMeshProUGUI concept;
+    public string originalTxt;
+    public string contextualTxt;
+    public GameObject newAgent;
+
+    //public void Start()
+    //{
+    //    concept.text = originalTxt;
+    //}
+
+    public void Update()
+    {
+        if (newAgent.activeInHierarchy == true)
+        {
+            concept.text = contextualTxt;
+        }
+        else if (newAgent.activeInHierarchy == false)
+        {
+            concept.text = originalTxt;
+
+        }
+    }
+}
