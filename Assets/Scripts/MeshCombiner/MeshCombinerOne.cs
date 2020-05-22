6d919e386615c51cf20877bb86b167d91cd0b66a
Eplundare <aelozanoa@gmail.com>
2020-05-22 21:42:37 +0200

    Optimized scene 18. Hid meshes. Couldn't CombineMeshes.

---
+++ Assets/Scripts/MeshCombiner/MeshCombinerOne.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Copy meshes from children into the parent's Mesh.
+// CombineInstance stores the list of meshes.  These are combined
+// and assigned to the attached Mesh.
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshCombinerOne : MonoBehaviour
+{
+    void Start()
+    {
+        Quaternion oldRot = transform.rotation;
+        Vector3 oldPos = transform.position;
+
+        transform.rotation = Quaternion.identity;
+        transform.position = Vector3.zero;
+
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].subMeshIndex = 0;
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+
+            i++;
+        }
+        //transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        //transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        //transform.gameObject.SetActive(true);
+
+        var meshFilter = transform.GetComponent<MeshFilter>();
+        meshFilter.mesh = new Mesh();
+        meshFilter.mesh.CombineMeshes(combine);
+        //GetComponent<MeshCollider>().sharedMesh = meshFilter.mesh;
+        meshFilter.mesh.RecalculateNormals();
+        transform.gameObject.SetActive(true);
+
+        transform.localScale = new Vector3(1, 1, 1);
+        //transform.rotation = Quaternion.identity;
+        //transform.position = Vector3.zero;
+        transform.rotation = oldRot;
+        transform.position = oldPos;
+    }
+}
