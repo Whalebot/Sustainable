c9863cc53541dd6e937f09e73ee422083c94ff97
Eplundare <aelozanoa@gmail.com>
2020-05-25 16:05:49 +0200

    New balance scene, easier money

---
+++ Assets/Scripts/ResourceReseter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceReseter : MonoBehaviour
+{
+    public Amount food;
+    public Amount ener;
+    public Amount wastem;
+    public Amount money;
+    public Amount pop;
+    public Amount approv;
+    public Amount pollut;
+
+    public float foodFloat;
+    public float enerFloat;
+    public float wastemFloat;
+    public float moneyFloat;
+    public float popFloat;
+    public float approvFloat;
+    public float pollutFloat;
+
+    public void ResetResources()
+    {
+        food.amountFloat = foodFloat;
+        ener.amountFloat = enerFloat;
+        wastem.amountFloat = wastemFloat;
+        money.amountFloat = moneyFloat;
+        pop.amountFloat = popFloat;
+        approv.amountFloat = approvFloat;
+        pollut.amountFloat = pollutFloat;
+
+    }
+}
---
+++ Assets/Scripts/SimpleObjectActivator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObjectActivator : MonoBehaviour
+{
+    public GameObject obj;
+
+    public void ActivateObj()
+    {
+        obj.gameObject.SetActive(true);
+    }
+
+    public void DeactivateObj()
+    {
+        obj.gameObject.SetActive(false);
+
+    }
+}
