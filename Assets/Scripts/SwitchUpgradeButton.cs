df630eb676466f0f89e2580fb7f5990582062a13
Eplundare <aelozanoa@gmail.com>
2020-05-26 13:40:01 +0200

    Built. Fixed locators shrink unshrink

---
+++ Assets/Scripts/SwitchUpgradeButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchUpgradeButton : MonoBehaviour
+{
+    public GameObject shrinkerButt;
+    public GameObject unshrinkerButt;
+    public bool shrinkerIsOn;
+
+    //public float waitTime;
+
+    public void SwitchButtons()
+    {
+        //StartCoroutine(SwitchWaitCoroutine());
+        shrinkerIsOn = !shrinkerIsOn;
+
+    }
+
+    public void Update()
+    {
+        if (shrinkerIsOn == true)
+        {
+            shrinkerButt.gameObject.SetActive(true);
+            unshrinkerButt.gameObject.SetActive(false);
+        }
+        else if (shrinkerIsOn ==false)
+        {
+            shrinkerButt.gameObject.SetActive(false);
+            unshrinkerButt.gameObject.SetActive(true);
+
+        }
+
+    }
+    //IEnumerator SwitchWaitCoroutine()
+    //{
+
+    //    yield return new WaitForSeconds(waitTime);
+    //    shrinkerIsOn = !shrinkerIsOn;
+
+    //}
+}
