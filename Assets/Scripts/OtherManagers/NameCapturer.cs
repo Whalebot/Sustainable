0fde60137f06379672c3293169795ddf708b9f32
Eplundare <aelozanoa@gmail.com>
2020-09-11 16:46:51 +0200

    Added NameCapturer.cs with "playerid", and StartScreen for player name.

---
+++ Assets/Scripts/OtherManagers/NameCapturer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NameCapturer : MonoBehaviour
+{
+    public bool needsRender;
+    public string playerid;
+    public TextMeshProUGUI inputFieldTxt;
+    public GameObject buttonOff;
+    public GameObject div;
+    public TMP_InputField nameInput;
+
+    public void Start()
+    {
+        if (needsRender == true)
+        {
+            div.gameObject.SetActive(true);
+        }
+    }
+
+    public void Capture()
+    {
+        playerid = inputFieldTxt.text;
+    }
+
+    public void Update()
+    {
+        
+
+        if (nameInput.text == "")
+        {
+            buttonOff.gameObject.SetActive(true);
+        }
+
+        else if (nameInput.text != "")
+        {
+            buttonOff.gameObject.SetActive(false);
+
+        }
+    }
+}
