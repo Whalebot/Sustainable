572e13fb004fab94869d7e6401297d802a4186ba
Eplundare <aelozanoa@gmail.com>
2020-10-01 11:56:47 +0200

    Added Small-Scale Upgrade Limit to 7
    Applied changes to gameObjects for Meat, Veggies, Insects, Algae, Energy, and Waste:
    
    Added gameObject: Limit Box as child of Concept. Txt: "Limit: 7".
    Added gameobject: ButtonOff (Limit), besides other ButtonOffs in TradeOffWindows parent.
    Added Small-Scale Limit MANAGERs in Other Managers parent.
    Added OnClick event in Manual Upg buttons (in TradeOffWindows): ButtonPlus.OnClick(CheckLimit()); Limit Managers have the CheckLimit() function.

---
+++ Assets/Scripts/OtherManagers/SmallScaleLimitManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class SmallScaleLimitManager : MonoBehaviour
+{
+    public bool isAi;
+    public AmountSimple amountSimple;
+    public float limit;
+    public GameObject limitButtonOff;
+
+    // Update is called once per frame
+    public void CheckLimit()
+    {
+        if (isAi == false)
+        {
+            if (amountSimple.simpleAmount >= limit)
+            {
+                limitButtonOff.gameObject.SetActive(true);
+            }
+        }
+       
+    }
+}
