31cf85da9b03e04655ae3af9c8f4ff6c87843073
Alexis Emmanuel Lozano Angulo <44468257+Eplundare@users.noreply.github.com>
2020-12-07 20:27:55 +0100

    Implemented all animations. (Verification is missing)
    Food variables and 3d objects linked to them are working properly. Check what with AI.
    
    Check Cataclysm visualizations with and without AI.

---
+++ Assets/Scripts/Visualization scripts/VisualizerAnimFoodCrisis.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizerAnimFoodCrisis : MonoBehaviour
+{
+    public string phase;
+    public TimeMachine timeMach;
+    public bool variableDescends;
+
+    public Animator animatorObj; 
+    public bool isCataclysmObj;
+
+
+    public float limit0;
+    public float midLimit12;   
+
+    void Update()
+    {
+        
+
+        if (isCataclysmObj == true)
+        {
+            
+                if (variableDescends == false) //So, variableAscends to infinity.
+                {
+                    if(timeMach.hungerCounter >= timeMach.hungerSevereThreshold 
+                    && timeMach.hungerCounter <= midLimit12
+                    )
+                    {
+                        animatorObj.SetBool("isCataclysm", true); 
+                    }
+                    else if(timeMach.hungerCounter < timeMach.hungerSevereThreshold 
+                    || timeMach.hungerCounter > midLimit12
+                    )
+                    {
+                        animatorObj.SetBool("isCataclysm", false); 
+                    }
+                    
+                }
+
+                
+            
+
+            
+        }
+       
+    }
+    
+}
---
+++ Assets/Scripts/Visualization scripts/VisualizerAnimation.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizerAnimation : MonoBehaviour
+{
+    public string phase;
+    public Amount trackingVariable;
+    public AmountSimple upgradePlusTrackingVariable;
+    public AutoAlert timeManagerAlerter; //Use only if cataclysm has a sourceAffector and a targetVariable
+    public bool variableDescends;
+
+    public Animator animatorObj; 
+    public bool isCataclysmObj;
+    public bool cataclysmUsesUpgradeSimpleAmount;
+
+
+    public float limit0;
+    public float midLimit12;   
+
+    void Update()
+    {
+        if (isCataclysmObj == false)
+        {
+            if (variableDescends == false) //So, variableAscends to infinity.
+            {
+                if(trackingVariable.amountFloat >= limit0 
+                && trackingVariable.amountFloat <= midLimit12)
+                {
+                    animatorObj.SetBool("isOn", true); 
+                }
+                else if(trackingVariable.amountFloat < limit0 
+                || trackingVariable.amountFloat > midLimit12)
+                {
+                    animatorObj.SetBool("isOn", false); 
+                }
+                
+            }
+
+            else if (variableDescends == true) //So, variableDescends.
+            {
+                if(trackingVariable.amountFloat <= limit0 
+                && trackingVariable.amountFloat >= midLimit12)
+                {
+                    animatorObj.SetBool("isOn", true); 
+                }
+                else if(trackingVariable.amountFloat > limit0 
+                || trackingVariable.amountFloat < midLimit12)
+                {
+                    animatorObj.SetBool("isOn", false); 
+                }
+                
+            }
+        }
+
+        else if (isCataclysmObj == true)
+        {
+            if (cataclysmUsesUpgradeSimpleAmount == false)
+            {
+
+                if (variableDescends == false) //So, variableAscends to infinity.
+                {
+                    if(trackingVariable.amountFloat >= limit0 
+                    && trackingVariable.amountFloat <= midLimit12
+                    && timeManagerAlerter.alertIsOn == true)
+                    {
+                        animatorObj.SetBool("isCataclysm", true); 
+                    }
+                    else if(/*trackingVariable.amountFloat < limit0*/ 
+                    // || trackingVariable.amountFloat > midLimit12
+                    /*&&*/ timeManagerAlerter.alertIsOn == false)
+                    {
+                        animatorObj.SetBool("isCataclysm", false); 
+                    }
+                    
+                }
+
+                else if (variableDescends == true) //So, variableDescends.
+                {
+                    if(trackingVariable.amountFloat <= limit0 
+                    && trackingVariable.amountFloat >= midLimit12
+                    && timeManagerAlerter.alertIsOn == true)
+                    {
+                        animatorObj.SetBool("isCataclysm", true); 
+                    }
+                    else if(/*trackingVariable.amountFloat > limit0 
+                    && trackingVariable.amountFloat < midLimit12*/
+                    timeManagerAlerter.alertIsOn == false)
+                    {
+                        animatorObj.SetBool("isCataclysm", false); 
+                    }
+                    
+                }
+            }
+
+            else if (cataclysmUsesUpgradeSimpleAmount == true)
+            {
+
+                if (variableDescends == false) //So, variableAscends to infinity.
+                {
+                    if(upgradePlusTrackingVariable.simpleAmount >= limit0 
+                    && upgradePlusTrackingVariable.simpleAmount <= midLimit12)
+                    {
+                        animatorObj.SetBool("isCataclysm", true); 
+                    }
+                    else if(upgradePlusTrackingVariable.simpleAmount < limit0 
+                    && upgradePlusTrackingVariable.simpleAmount > midLimit12)
+                    {
+                        animatorObj.SetBool("isCataclysm", false); 
+                    }
+                    
+                }
+
+                else if (variableDescends == true) //So, variableDescends.
+                {
+                    if(upgradePlusTrackingVariable.simpleAmount <= limit0 
+                    && upgradePlusTrackingVariable.simpleAmount >= midLimit12)
+                    {
+                        animatorObj.SetBool("isCataclysm", true); 
+                    }
+                    else if(upgradePlusTrackingVariable.simpleAmount > limit0 
+                    && upgradePlusTrackingVariable.simpleAmount < midLimit12)
+                    {
+                        animatorObj.SetBool("isCataclysm", false); 
+                    }
+                    
+                }
+            }
+        }
+       
+    }
+    
+}
